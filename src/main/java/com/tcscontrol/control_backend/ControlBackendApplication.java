7a32993d64f4d328bf9281644cb004f1b4556b40
fael.home <rafael.menezes@aluno.usj.edu.br>
2023-08-13 01:32:09 -0300

    star project

---
+++ src/main/java/com/tcscontrol/control_backend/ControlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tcscontrol.control_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcscontrol/control_backend/ControlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcscontrol.control_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
