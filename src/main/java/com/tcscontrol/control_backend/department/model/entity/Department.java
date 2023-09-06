5d58990b558186cab8ed59463e0c0ffd291116e6
Jose-Edston <joseedston@gmail.com>
2023-09-05 21:24:33 -0300

    correction department

--- src/main/java/com/tcscontrol/control_backend/department/model/entity/Department.java
+++ src/main/java/com/tcscontrol/control_backend/department/model/entity/Department.java
@@ -1,38 +1,41 @@
 package com.tcscontrol.control_backend.department.model.entity;
 
-import org.springframework.data.annotation.Id;
+import java.io.Serializable;
+
 
 import com.tcscontrol.control_backend.pessoa.user.model.entity.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "DEPARTAMENTOS")
-@EqualsAndHashCode(callSuper = false)
 @Data
+@Table(name = "DEPARTAMENTOS")
 @AllArgsConstructor
 @NoArgsConstructor
-public class Department {
+public class Department implements Serializable {
+
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_departamento")
     private Long id;
 
     @Column(name = "nm_departamento")
     private String nmDepartamento;
 
-    @ManyToOne
-    @Column(name = "id_usuario")
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "id_usuario", nullable = true)
     private User user;
     
 }
