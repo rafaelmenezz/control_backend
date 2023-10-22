dabf1e9868da323b3854a97a88a38ccdc2295345
rafaelmenezz <faelsmenezes@gmail.com>
2023-10-22 13:31:55 -0300

    Implementado UserResponse

---
+++ src/main/java/com/tcscontrol/control_backend/pessoa/user/model/dto/UserResponse.java
@@ -0,0 +1,19 @@
+package com.tcscontrol.control_backend.pessoa.user.model.dto;
+
+import java.util.List;
+
+import com.tcscontrol.control_backend.contacts.model.ContactsDTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record UserResponse(
+    Long id,
+    @NotBlank @NotNull String nmUsuario,
+    String nrMatricula, 
+    @NotBlank @NotNull String nrCpf,
+    String ftFoto, 
+    @NotNull List<ContactsDTO> contacts
+) {
+    
+}
