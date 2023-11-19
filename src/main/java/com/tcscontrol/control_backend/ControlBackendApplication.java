1229aba2e54184d2b7d556d215ea90b965148586
rafaelmenezz <faelsmenezes@gmail.com>
2023-11-19 19:12:23 -0300

    Implementado Tarefas Agendadas

--- src/main/java/com/tcscontrol/control_backend/ControlBackendApplication.java
+++ src/main/java/com/tcscontrol/control_backend/ControlBackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.tcscontrol.control_backend.file.StorageService;
 import com.tcscontrol.control_backend.file.model.entity.StorageProperties;
@@ -12,6 +13,7 @@ import com.tcscontrol.control_backend.file.model.entity.StorageProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
+@EnableScheduling
 public class ControlBackendApplication {
 
 	public static void main(String[] args) {
