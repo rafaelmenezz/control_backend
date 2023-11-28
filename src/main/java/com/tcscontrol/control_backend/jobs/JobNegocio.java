8b0463ac2b12f7bbed5a258282fb55956326ef7b
Jose-Edston <joseedston@gmail.com>
2023-11-27 21:12:10 -0300

    fix: ajustes jaime

--- src/main/java/com/tcscontrol/control_backend/jobs/JobNegocio.java
+++ src/main/java/com/tcscontrol/control_backend/jobs/JobNegocio.java
@@ -2,8 +2,8 @@ package com.tcscontrol.control_backend.jobs;
 
 public interface JobNegocio {
     
-    static final String ASSUNTO_EMAIL_GESTOR_VENCER = "Suas Requisições Vence Amanhã!"; 
-    static final String MSG_GESTOR_REQUISICOES_VENCER = "Você possui patrimônios que deverão ser entregue amanhã. Caso for de seu interesse entre contato com administrador para renovar o seu pedido. Segue os patrimônio;";
+    static final String ASSUNTO_EMAIL_GESTOR_VENCER = "Patrimônios Com  Prazo de Entrega Próximo A vencer!"; 
+    static final String MSG_GESTOR_REQUISICOES_VENCER = "Você possui patrimônios que devem ser entregues. Caso for de seu interesse entre contato com administrador para renovar o seu pedido. Segue os patrimônios;";
     
     static final String ASSUNTO_EMAIL_ADMIN = "Informações dos Patrimônio DATA!"; 
     static final String MSG_ADMIN_REQUISICOES = "Segue informações dos patrimônios do dia:";
